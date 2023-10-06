29a78cf93b2f2e58b11f664f3b3b3c2898a6c0c0
smit shah <shahsmit293@gmail.com>
2023-10-06 13:52:17 +0000

    First changes

--- src/api/models.py
+++ src/api/models.py
@@ -1,8 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__='user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
@@ -16,4 +18,66 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Characters(db.Model):
+    __tablename__='characters'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    gender = db.Column(db.String(120), nullable=False)
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            'gender':self.gender
+            # do not serialize the password, its a security breach
+        }
+
+class Locations(db.Model):
+    __tablename__='locations'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    type = db.Column(db.String(120), nullable=False)
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            'gender':self.type
+            # do not serialize the password, its a security breach
+        }
+
+class Favourites(db.Model):
+    __tablename__='favourites'
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    people_id = db.Column(db.Integer, db.ForeignKey('characters.id'))
+    planet_id = db.Column(db.Integer, db.ForeignKey('locations.id'))
+
+    def __init__(self,user_id,people_id=None,planet_id=None):
+        self.user_id=user_id
+        self.people_id=people_id
+        self.planet_id=planet_id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "people_id":self.people_id,
+            "planet_id":self.planet_id
+        }
+    
+    def serialize_planet(self):
+        return {
+            "id":self.id,
+            "user_id": self.user_id,
+            "planet_id":self.planet_id
+        }
+    
+    def serialize_people(self):
+        return {
+            "id":self.id,
+            "user_id": self.user_id,
+            "planet_id":self.people_id
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User,Characters,Locations,Favourites
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -16,3 +16,82 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route('/people',methods=['GET'])
+def handle_peopele():
+    peoples=Characters.query.all()
+    people_dictoneries=[]
+    for people in peoples:
+        people_dictoneries.append(people.serialize())
+    return jsonify(people_dictoneries),200
+
+@api.route('/people/<id>',methods=['GET'])
+def handle_individual_peopele(id):
+    all_peoples=Characters.query.get(id)
+    return jsonify(all_peoples.serialize()),200
+
+@api.route('/planet',methods=['GET'])
+def handle_planet():
+    planets=Locations.query.all()
+    planets_dictoneries=[]
+    for planet in planets:
+        planets_dictoneries.append(planet.serialize())
+    return jsonify(planets_dictoneries),200
+
+@api.route('/planet/<id>',methods=['GET'])
+def handle_individual_planet(id):
+    all_planets=Locations.query.get(id)
+    return jsonify(all_planets.serialize()),200
+
+@api.route('/users',methods=['GET'])
+def handle_user():
+    users=User.query.all()
+    user_dictoneries=[]
+    for user in users:
+        user_dictoneries.append(user.serialize())
+    return jsonify(user_dictoneries),200
+
+@api.route('/users/favorites',methods=['GET'])
+def all_favorites():
+    favorites=Favourites.query.all()
+    favorite_dictoneries=[]
+    for favorite in favorites:
+        favorite_dictoneries.append(favorite.serialize())
+    return jsonify(favorite_dictoneries),200
+
+@api.route('/favorite/planet/<planet_id>',methods=['POST'])
+def favorite_planet(planet_id):
+    body=request.json
+    single_planet=Favourites(
+        user_id=body["user_id"],
+        planet_id=planet_id
+    )
+    db.session.add(single_planet)
+    db.session.commit()
+    return single_planet.serialize_planet()
+
+@api.route('/favorite/people/<people_id>',methods=['POST'])
+def favorite_person(people_id):
+    body=request.json
+    single_people=Favourites(
+        user_id=body["user_id"],
+        people_id=people_id
+    )
+    db.session.add(single_people)
+    db.session.commit()
+    return single_people.serialize_people()
+
+@api.route('/favorite/planet/<planet_id>',methods=['DELETE'])
+def delete_planet(planet_id):
+    delete_single_planet=Favourites.query.get(planet_id)
+    db.session.delete(delete_single_planet)
+    db.session.commit()
+    return None
+
+@api.route('/favorite/people/<people_id>',methods=['DELETE'])
+def delete_person(people_id):
+    delete_single_people=Favourites.query.get(people_id)
+    db.session.delete(delete_single_people)
+    db.session.commit()
+    return None
